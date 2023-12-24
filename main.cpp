02b66641bacdc00636e5bb4ffdc55ae225f4c3e9
Moosayed1 <mohamed1212213183@gmail.com>
2023-12-24 16:14:17 +0200

    fix cmake file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include "AVLTree.h"
+#include "treeGram.h"
 // #include "hashTable.h"
 using namespace std;
 int main()
 {
+    treeGram obj;
     cout<<"Ahmed Mohsen"<<endl;
     return 0;
 }
